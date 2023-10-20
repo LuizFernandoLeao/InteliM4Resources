88e98a0fec12d340aac148ef5d47505868622a57
LuizFernandoLeao <luiz.leao@sou.inteli.edu.br>
2023-10-20 16:55:51 -0300

    ponderada semana 1

---
+++ SEMANA_01/01_CODIGOS/luiz_ponderada_semana_1.ino
@@ -0,0 +1,16 @@
+// Código Luiz - Ponderada Semana 1
+
+// C++ code
+//
+void setup()
+{
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(10, HIGH);
+  delay(500); // Espera meio segundo
+  digitalWrite(10, LOW);
+  delay(500); // Espera meio segundo
+}
